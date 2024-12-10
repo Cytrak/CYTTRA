a5630877876152456691e1cf7b7898f2045c8fd8
Cytrak <149499127+Cytrak@users.noreply.github.com>
2024-12-10 10:51:34 +0300

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -1,10 +1,13 @@
-const { cmd } = require('../command');
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
 
 cmd({
-    pattern: "hack",
-    desc: "Displays a dynamic and playful 'Hacking' message for fun.",
-    category: "fun",
-    react: "☣️",
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "Hack prank",
+    category: "main",
+    react: "⚔️",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
